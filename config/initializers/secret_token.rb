9cf9d031f1376d5c26e97abc0de7daba54bcf0ac
jherrera <jherrera@visionps.com>
2012-12-21 09:46:49 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Justareminder::Application.config.secret_token = 'ea947c5c0ef8589dd39b634ff23a005377d0f2b74f523462c9964372c148e04f66074ebf457dab4ed53635de7fe5477648b2861043faf1037fa6e6b5e4059c8d'
